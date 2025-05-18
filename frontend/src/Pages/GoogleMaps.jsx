dea696f400ad7b7a8b3240e98f23366ba7873e76
Audelo03 <diegoaudelopersonal@gmail.com>
2025-05-18 02:30:57 -0600

    ca

--- frontend/src/Pages/GoogleMaps.jsx
+++ frontend/src/Pages/GoogleMaps.jsx
@@ -48,12 +48,19 @@ const museoIconSvgString = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32px" height="32px">
   <defs>
     <linearGradient id="museoGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color:#795548; stop-opacity:1" />
-      <stop offset="100%" style="stop-color:#5D4037; stop-opacity:1" />
+      <stop offset="0%" style="stop-color:#3E2723; stop-opacity:1" />
+      <stop offset="50%" style="stop-color:#5D4037; stop-opacity:1" />
+      <stop offset="100%" style="stop-color:#1C0E0A; stop-opacity:1" />
     </linearGradient>
+    <radialGradient id="museoHighlight" cx="50%" cy="30%" r="70%">
+      <stop offset="0%" style="stop-color:#6D4C41; stop-opacity:0.8" />
+      <stop offset="100%" style="stop-color:#3E2723; stop-opacity:0.2" />
+    </radialGradient>
   </defs>
   <path fill="url(#museoGradient)" d="M22 11V9L12 2L2 9v2h2v9H2v2h20v-2h-2v-9zm-4 9H6V9h12z"/>
-  <path fill="#FFFFFF" d="M6 20h12V9H6zm2-9h2l2 3l2-3h2v7h-2v-4l-2 3l-2-3v4H8z" opacity=".7"/>
+  <path fill="url(#museoHighlight)" d="M22 11V9L12 2L2 9v2h2v9H2v2h20v-2h-2v-9zm-4 9H6V9h12z"/>
+  <path fill="#D7CCC8" d="M6 20h12V9H6zm2-9h2l2 3l2-3h2v7h-2v-4l-2 3l-2-3v4H8z" opacity=".9"/>
+  <circle fill="#8D6E63" cx="12" cy="6" r="1" opacity="0.6"/>
 </svg>`;
 
 const restauranteIconSvgString = `
@@ -61,9 +68,18 @@ const restauranteIconSvgString = `
   <title>Icono de Restaurante</title>
   <defs>
     <linearGradient id="restauranteGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color:#FF8A65; stop-opacity:1" /> <stop offset="100%" style="stop-color:#F4511E; stop-opacity:1" /> </linearGradient>
+      <stop offset="0%" style="stop-color:#4E342E; stop-opacity:1" />
+      <stop offset="30%" style="stop-color:#6D4C41; stop-opacity:1" />
+      <stop offset="70%" style="stop-color:#5D4037; stop-opacity:1" />
+      <stop offset="100%" style="stop-color:#3E2723; stop-opacity:1" />
+    </linearGradient>
+    <linearGradient id="restauranteAccent" x1="0%" y1="0%" x2="100%" y2="100%">
+      <stop offset="0%" style="stop-color:#8D6E63; stop-opacity:0.7" />
+      <stop offset="100%" style="stop-color:#4E342E; stop-opacity:0.3" />
+    </linearGradient>
   </defs>
-  <path fill="url(#restauranteGradient)" d="M342.7 223.94h14.87a79.48 79.48 0 0 0 56.58-23.44L496 118l-22.22-22.4l-83.58 83.58l-17.37-17.37l83.58-83.59l-23-22.31l-83.27 83.26l-17.32-17.37l83.58-83.59L394 16l-82.5 81.85a79.5 79.5 0 0 0-23.44 56.59v14.86l-43.13 43.13L48 16C3.72 70.87 29.87 171.71 79.72 221.57l85.5 85.5c26.55 26.55 31.82 28.92 61.94 16.8c6.49-2.61 8.85-2.32 14.9 3.72l13 12.13c2.93 3 3 3.88 3 9.62v5.54c0 21.08 13.48 33.2 22.36 42.24L384 496l72-72l-156.43-156.93Z" />
+  <path fill="url(#restauranteGradient)" d="M342.7 223.94h14.87a79.48 79.48 0 0 0 56.58-23.44L496 118l-22.22-22.4l-83.58 83.58l-17.37-17.37l83.58-83.59l-23-22.31l-83.27 83.26l-17.32-17.37l83.58-83.59L394 16l-82.5 81.85a79.5 79.5 0 0 0-23.44 56.59v14.86l-43.13 43.13L48 16C3.72 70.87 29.87 171.71 79.72 221.57l85.5 85.5c26.55 26.55 31.82 28.92 61.94 16.8c6.49-2.61 8.85-2.32 14.9 3.72l13 12.13c2.93 3 3 3.88 3 9.62v5.54c0 21.08 13.48 33.2 22.36 42.24L384 496l72-72l-156.43-156.93 Z" />
+  <path fill="url(#restauranteAccent)" d="M342.7 223.94h14.87a79.48 79.48 0 0 0 56.58-23.44L496 118l-22.22-22.4l-83.58 83.58l-17.37-17.37l83.58-83.59l-23-22.31l-83.27 83.26l-17.32-17.37l83.58-83.59L394 16l-82.5 81.85a79.5 79.5 0 0 0-23.44 56.59v14.86l-43.13 43.13L48 16C3.72 70.87 29.87 171.71 79.72 221.57l85.5 85.5c26.55 26.55 31.82 28.92 61.94 16.8c6.49-2.61 8.85-2.32 14.9 3.72l13 12.13c2.93 3 3 3.88 3 9.62v5.54c0 21.08 13.48 33.2 22.36 42.24L384 496l72-72l-156.43-156.93 Z" />
   <path fill="url(#restauranteGradient)" d="M227.37 354.59c-29.82 6.11-48.11 11.74-83.08-23.23c-.56-.56-1.14-1.1-1.7-1.66l-19.5-19.5L16 416l80 80l144-144Z" />
 </svg>`;
 
@@ -72,7 +88,15 @@ const monumentoHistoricoIconSvgString = `
   <title>Monumento Histórico</title>
   <defs>
     <linearGradient id="monumentoGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color:#BDBDBD;"/> <stop offset="100%" style="stop-color:#757575;"/> </linearGradient>
+      <stop offset="0%" style="stop-color:#424242; stop-opacity:1"/>
+      <stop offset="30%" style="stop-color:#616161; stop-opacity:1"/>
+      <stop offset="70%" style="stop-color:#424242; stop-opacity:1"/>
+      <stop offset="100%" style="stop-color:#212121; stop-opacity:1"/>
+    </linearGradient>
+    <linearGradient id="monumentoShadow" x1="100%" y1="0%" x2="0%" y2="100%">
+      <stop offset="0%" style="stop-color:#757575; stop-opacity:0.3"/>
+      <stop offset="100%" style="stop-color:#212121; stop-opacity:0.7"/>
+    </linearGradient>
   </defs>
   <g fill="url(#monumentoGradient)">
     <path d="M23.025 12.49H9.005v13.99h14.02z"/>
@@ -83,11 +107,18 @@ const monumentoHistoricoIconSvgString = `
     <path d="M20.315 13.46h-2.99v12.1h2.99z"/>
     <path d="M25.975 13.46h-2.99v12.1h2.99z"/>
     <path d="M15.326 3.01L3.366 9.75c-.36.2-.22.76.2.76h24.88c.42 0 .56-.55.2-.76l-11.96-6.74c-.42-.24-.94-.24-1.36 0"/>
+  </g>
+  <g fill="url(#monumentoShadow)">
+    <path d="M23.025 12.49H9.005v13.99h14.02z" opacity="0.3"/>
+    <path d="M26.975 13.46h-2.99v12.1h2.99z" opacity="0.5"/>
+    <path d="M21.315 13.46h-2.99v12.1h2.99z" opacity="0.4"/>
+    <path d="M15.655 13.46h-2.99v12.1h2.99z" opacity="0.3"/>
+  </g>
+  <g fill="#9E9E9E">
     <path d="M5.015 12.49h4.99v1h-4.99z" />
     <path d="M10.665 12.49h4.99v1h-4.99z" />
     <path d="M16.325 12.49h4.99v1h-4.99z" />
     <path d="M21.985 12.49h4.99v1h-4.99z" />
-
     <path d="M5.015 25.48h4.99v1h-4.99z" />
     <path d="M10.665 25.48h4.99v1h-4.99z" />
     <path d="M16.325 25.48h4.99v1h-4.99z" />
@@ -100,15 +131,35 @@ const naturalezaIconSvgString = `
   <title>Naturaleza</title>
   <defs>
     <linearGradient id="follajeGradient" x1="50%" y1="0%" x2="50%" y2="100%">
-      <stop offset="0%" style="stop-color:#66BB6A;" /> <stop offset="100%" style="stop-color:#388E3C;" /> </linearGradient>
+      <stop offset="0%" style="stop-color:#2E7D32;" />
+      <stop offset="40%" style="stop-color:#388E3C;" />
+      <stop offset="80%" style="stop-color:#1B5E20;" />
+      <stop offset="100%" style="stop-color:#0D2818;" />
+    </linearGradient>
     <linearGradient id="troncoGradient" x1="50%" y1="0%" x2="50%" y2="100%">
-      <stop offset="0%" style="stop-color:#8D6E63;" /> <stop offset="100%" style="stop-color:#5D4037;" /> </linearGradient>
+      <stop offset="0%" style="stop-color:#4E342E;" />
+      <stop offset="50%" style="stop-color:#5D4037;" />
+      <stop offset="100%" style="stop-color:#3E2723;" />
+    </linearGradient>
+    <radialGradient id="follajeHighlight" cx="40%" cy="20%" r="60%">
+      <stop offset="0%" style="stop-color:#4CAF50; stop-opacity:0.8" />
+      <stop offset="100%" style="stop-color:#1B5E20; stop-opacity:0.2" />
+    </radialGradient>
   </defs>
   <path fill="url(#troncoGradient)" d="M25 52.1h14V64H25z"/>
+  <path fill="#3E2723" d="M27 54.1h10V62H27z" opacity="0.6"/>
+  
   <path fill="url(#follajeGradient)" d="M32 34.9L2 55.1s14.5 3.4 30 3.4s30-3.4 30-3.4z"/>
-  <path fill="url(#follajeGradient)" d="M32 23.6L7 43.8s12.1 3.4 25 3.4s25-3.4 25-3.4z" style="opacity:0.9"/>
-  <path fill="url(#follajeGradient)" d="M32 12.3L12 32.5s9.7 3.4 20 3.4s20-3.4 20-3.4z" style="opacity:0.8"/>
-  <path fill="url(#follajeGradient)" d="M32 1L17 20.8s7.2 3.8 15 3.8s15-3.8 15-3.8z" style="opacity:0.7"/>
+  <path fill="url(#follajeHighlight)" d="M32 34.9L2 55.1s14.5 3.4 30 3.4s30-3.4 30-3.4z" opacity="0.5"/>
+  
+  <path fill="url(#follajeGradient)" d="M32 23.6L7 43.8s12.1 3.4 25 3.4s25-3.4 25-3.4z" style="opacity:0.95"/>
+  <path fill="url(#follajeHighlight)" d="M32 23.6L7 43.8s12.1 3.4 25 3.4s25-3.4 25-3.4z" style="opacity:0.4"/>
+  
+  <path fill="url(#follajeGradient)" d="M32 12.3L12 32.5s9.7 3.4 20 3.4s20-3.4 20-3.4z" style="opacity:0.9"/>
+  <path fill="url(#follajeHighlight)" d="M32 12.3L12 32.5s9.7 3.4 20 3.4s20-3.4 20-3.4z" style="opacity:0.3"/>
+  
+  <path fill="url(#follajeGradient)" d="M32 1L17 20.8s7.2 3.8 15 3.8s15-3.8 15-3.8z" style="opacity:0.85"/>
+  <path fill="url(#follajeHighlight)" d="M32 1L17 20.8s7.2 3.8 15 3.8s15-3.8 15-3.8z" style="opacity:0.25"/>
 </svg>`;
 
 const gobiernoIconSvgString = `
@@ -116,9 +167,24 @@ const gobiernoIconSvgString = `
   <title>Dependencia de Gobierno</title>
   <defs>
     <linearGradient id="gobiernoGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color:#78909C;"/> <stop offset="100%" style="stop-color:#455A64;"/> </linearGradient>
+      <stop offset="0%" style="stop-color:#37474F; stop-opacity:1"/>
+      <stop offset="30%" style="stop-color:#455A64; stop-opacity:1"/>
+      <stop offset="70%" style="stop-color:#37474F; stop-opacity:1"/>
+      <stop offset="100%" style="stop-color:#263238; stop-opacity:1"/>
+    </linearGradient>
+    <linearGradient id="gobiernoAccent" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" style="stop-color:#546E7A; stop-opacity:0.6"/>
+      <stop offset="100%" style="stop-color:#263238; stop-opacity:0.8"/>
+    </linearGradient>
+    <radialGradient id="gobiernoHighlight" cx="50%" cy="30%" r="70%">
+      <stop offset="0%" style="stop-color:#607D8B; stop-opacity:0.4"/>
+      <stop offset="100%" style="stop-color:#263238; stop-opacity:0.1"/>
+    </radialGradient>
   </defs>
   <path fill="url(#gobiernoGradient)" d="M13.032 2.336a1.75 1.75 0 0 0-2.064 0L3.547 7.75c-.977.712-.474 2.256.734 2.258H16V10h3v.007h.719c1.208-.002 1.71-1.546.734-2.258zM12 5.26a1 1 0 1 1 0 2a1 1 0 0 1 0-2m-.75 10.149q-.137.28-.2.594h-1.8v-4.997h2zm3.5-3.972A2.75 2.75 0 0 0 13 14v.05a3 3 0 0 0-.25.064v-3.108h2zM11 17.003V20.5H3.75a.75.75 0 0 1-.75-.75v-.5a2.25 2.25 0 0 1 2.25-2.248zm-5.5-1h2.25v-4.996H5.5zM14 15h-.5a1.5 1.5 0 0 0-1.5 1.5V18h2.5v-.25a.75.75 0 0 1 1.5 0V18h3v-.25a.75.75 0 0 1 1.5 0V18H23v-1.5a1.5 1.5 0 0 0-1.5-1.5H21v-1a1.75 1.75 0 0 0-1.75-1.75h-3.5A1.75 1.75 0 0 0 14 14zm1.5-1a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v1h-4zm-2 9a1.5 1.5 0 0 1-1.5-1.5v-2h2.5v.75a.75.75 0 0 0 1.5 0v-.75h3v.75a.75.75 0 0 0 1.5 0v-.75H23v2a1.5 1.5 0 0 1-1.5 1.5z"/>
+  <path fill="url(#gobiernoAccent)" d="M13.032 2.336a1.75 1.75 0 0 0-2.064 0L3.547 7.75c-.977.712-.474 2.256.734 2.258H16V10h3v.007h.719c1.208-.002 1.71-1.546.734-2.258z"/>
+  <path fill="url(#gobiernoHighlight)" d="M13.032 2.336a1.75 1.75 0 0 0-2.064 0L3.547 7.75c-.977.712-.474 2.256.734 2.258H16V10h3v.007h.719c1.208-.002 1.71-1.546.734-2.258zM12 5.26a1 1 0 1 1 0 2a1 1 0 0 1 0-2"/>
+  <circle fill="#78909C" cx="12" cy="6.26" r="0.8" opacity="0.7"/>
 </svg>`;
 
 const hospedajeIconSvgString = `
@@ -126,24 +192,52 @@ const hospedajeIconSvgString = `
   <title>Hospedaje</title>
   <defs>
     <linearGradient id="hotelGradientBody" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color:#FFF3E0;" /> <stop offset="100%" style="stop-color:#FFCC80;" /> </linearGradient>
-     <linearGradient id="hotelGradientRoof" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color:#BF360C;" /> <stop offset="100%" style="stop-color:#DD2C00;" /> </linearGradient>
-     <linearGradient id="hotelGradientWindow" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" style="stop-color:#81D4FA;" /> <stop offset="100%" style="stop-color:#29B6F6;" /> </linearGradient>
+      <stop offset="0%" style="stop-color:#3E2723;" />
+      <stop offset="30%" style="stop-color:#5D4037;" />
+      <stop offset="70%" style="stop-color:#4E342E;" />
+      <stop offset="100%" style="stop-color:#2C1810;" />
+    </linearGradient>
+    <linearGradient id="hotelGradientRoof" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop offset="0%" style="stop-color:#8B0000;" />
+      <stop offset="50%" style="stop-color:#B71C1C;" />
+      <stop offset="100%" style="stop-color:#4A0000;" />
+    </linearGradient>
+    <linearGradient id="hotelGradientWindow" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop offset="0%" style="stop-color:#1565C0;" />
+      <stop offset="50%" style="stop-color:#1976D2;" />
+      <stop offset="100%" style="stop-color:#0A3F73;" />
+    </linearGradient>
+    <linearGradient id="hotelGradientDoor" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop offset="0%" style="stop-color:#4E342E;" />
+      <stop offset="100%" style="stop-color:#2C1810;" />
+    </linearGradient>
+    <radialGradient id="buildingHighlight" cx="50%" cy="20%" r="80%">
+      <stop offset="0%" style="stop-color:#6D4C41; stop-opacity:0.4"/>
+      <stop offset="100%" style="stop-color:#3E2723; stop-opacity:0.1"/>
+    </radialGradient>
   </defs>
+  
   <path fill="url(#hotelGradientBody)" d="m20.6 51.7l-10.7.01s-4.53-7.4-4.93-7.98c-.53-.75-1.72-.63-2.11-.57c-.67.09-.14 1.17.61 2.25s3.94 6.43 3.94 6.43v72.02h113.22l.09-71.78s3.98-6.54 4.37-7.19s.98-1.69.26-1.73c-.78-.04-1.68-.18-2.45 1.03c-.88 1.39-4.58 7.16-4.58 7.16h-10.8V40.17h-16.6s-.59-22.84-27.15-22.68c-26.55.17-26.25 22.66-26.25 22.66H20.74c0 .01-.14 11.35-.14 11.55z"/>
+  
+  <path fill="url(#buildingHighlight)" d="m20.6 51.7l-10.7.01s-4.53-7.4-4.93-7.98c-.53-.75-1.72-.63-2.11-.57c-.67.09-.14 1.17.61 2.25s3.94 6.43 3.94 6.43v72.02h113.22l.09-71.78s3.98-6.54 4.37-7.19s.98-1.69.26-1.73c-.78-.04-1.68-.18-2.45 1.03c-.88 1.39-4.58 7.16-4.58 7.16h-10.8V40.17h-16.6s-.59-22.84-27.15-22.68c-26.55.17-26.25 22.66-26.25 22.66H20.74c0 .01-.14 11.35-.14 11.55z"/>
+  
   <path fill="url(#hotelGradientBody)" d="M9.86 56.19h10.59v44.28l-10.68.1zm13.28-11.52v44.38l81.98-.09v-44.1H85.81s1.38-22.41-21.45-22.65c-23.67-.26-21.91 22.28-21.91 22.28z"/>
   <path fill="url(#hotelGradientBody)" d="M118.25 56.56h-10.32v43.9h10.22z"/>
-
+  
   <path fill="url(#hotelGradientRoof)" d="m3.68 43.11l17.03.21s-.07-10.03 0-10.78c.07-.74.67-.97 1.63-.97h11.52s4.21-19.72 30.67-19.58c25.08.13 29.89 19.36 29.89 19.36s11.22.15 11.96.15s1.11.59 1.11 1.41v10.26h16.78l-.8 1.93l-4.9 8.25l-109.24-.01z"/>
-  <path fill="#D84315" d="M19.42 91.91s.07 5.86.12 6.83s-.11 1.89 2.06 1.91c2.13.02 84.69.04 85.76.04s2.28-.61 2.38-2.14c.1-1.52.05-6.43.05-6.43l-46.36-1.6z"/>
+  
+  <path fill="#6B1A00" d="M19.42 91.91s.07 5.86.12 6.83s-.11 1.89 2.06 1.91c2.13.02 84.69.04 85.76.04s2.28-.61 2.38-2.14c.1-1.52.05-6.43.05-6.43l-46.36-1.6z"/>
   
   <path fill="url(#hotelGradientWindow)" d="M46.9 37.64h8.64v10H46.9zm26.15 0h8.64v10h-8.64z M46.9 50.64h8.64v10H46.9zm26.15 0h8.64v10h-8.64z M46.9 63.64h8.64v10H46.9zm26.15 0h8.64v10h-8.64z"/>
   <path fill="url(#hotelGradientWindow)" d="M26.74 47.06h6.38v10.06h-6.38zm-.03 14.02h6.38v10.06h-6.38z M95.68 46.61h6.38v10.06h-6.38zm-.03 14.02h6.38v10.06h-6.38z"/>
   
-  <path fill="#A1887F" d="m50.22 99.45l-.03 24.75H78.2l.03-25.51z"/>
-  <path fill="#795548" d="M52.8 102.66v18.63h9.88v-18.57zm12.71.19h9.76l.25 18.44h-9.94z"/>
+  <!-- Window frames -->
+  <path fill="#0A3F73" d="M46.4 37.14h9.64v11H46.4zm26.15 0h9.64v11h-9.64z M46.4 50.14h9.64v11H46.4zm26.15 0h9.64v11h-9.64z M46.4 63.14h9.64v11H46.4zm26.15 0h9.64v11h-9.64z" opacity="0.8"/>
+  
+  <path fill="url(#hotelGradientDoor)" d="m50.22 99.45l-.03 24.75H78.2l.03-25.51z"/>
+  <path fill="#2C1810" d="M52.8 102.66v18.63h9.88v-18.57zm12.71.19h9.76l.25 18.44h-9.94z"/>
+  <circle fill="#8D6E63" cx="55" cy="112" r="0.8" opacity="0.8"/>
+  <circle fill="#8D6E63" cx="71" cy="112" r="0.8" opacity="0.8"/>
 </svg>`;
 
 
